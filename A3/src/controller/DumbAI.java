6052bda13fa4d98cd0520bc5b536185b326d0635
Sophia <Sophia@dhcp-rhodes-1744.redrover.cornell.edu>
2019-07-13 12:49:21 -0400

    Pair programmed with Julia Chang: added implementation for DumbAI

--- A3/src/controller/DumbAI.java
+++ A3/src/controller/DumbAI.java
@@ -1,5 +1,9 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Board;
 import model.Game;
 import model.Location;
 import model.NotImplementedException;
@@ -10,11 +14,14 @@ import model.Player;
  * smallest column number from the row with the smallest row number.
  */
 public class DumbAI extends Controller {
+	
+	private int row; 
+	private int column;
 
 	public DumbAI(Player me) {
 		super(me);
-		// TODO Auto-generated constructor stub
-		throw new NotImplementedException();
+		this.row = 0;
+		this.column = 0;
 	}
 
 	protected @Override Location nextMove(Game g) {
@@ -22,6 +29,39 @@ public class DumbAI extends Controller {
 		// nicely when competing different AIs against each other.
 		
 		// TODO Auto-generated method stub
-		throw new NotImplementedException();
+		List<Location> available = new ArrayList<Location>();
+		
+		// find available moves
+		for (Location loc : Board.LOCATIONS) {
+			if (g.getBoard().get(loc) == null && available.size() == 0) {
+				available.add(loc);
+			}
+			
+			//iterate backward or forward?
+			for (int i = 0; i < available.size(); i++) {
+				if ((g.getBoard().get(loc)== null) && (loc.row < available.get(i).row) && (loc.col < available.get(i).col)) {
+					available.add(i-1, loc);
+					i-=2;
+				} else if ((g.getBoard().get(loc)== null) && (loc.row == available.get(i).row) && (loc.col == available.get(i).col-1))  {
+					available.add(i-1,loc);
+					i-=2;
+				} else if ((g.getBoard().get(loc)== null) && (loc.row == available.get(i).row +1) && (loc.col == available.get(i).col-1))  {
+					available.add(i-1,loc);
+					i-=2;
+				} else {
+					available.add(loc);
+				}
+			}
+		}
+		
+		// wait a bit
+		delay();
+
+		// choose a random move
+		if (!available.isEmpty())
+			//return available.get(random.nextInt(available.size()));
+			return available.get(0);
+
+		return null;
 	}
 }
