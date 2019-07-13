ba9b3b00261cafe864e750fa8f60aaea15790c69
Julia Chang <jjc438@cornell.edu>
2019-07-13 13:22:48 -0400

    pair programming with Sophia He: fixed DumbAI

--- A3/src/controller/DumbAI.java
+++ A3/src/controller/DumbAI.java
@@ -6,7 +6,7 @@ import java.util.List;
 import model.Board;
 import model.Game;
 import model.Location;
-import model.NotImplementedException;
+//import model.NotImplementedException;
 import model.Player;
 
 /**
@@ -14,15 +14,10 @@ import model.Player;
  * smallest column number from the row with the smallest row number.
  */
 public class DumbAI extends Controller {
-	
-	//invariants: row and column
-	private int row; 
-	private int column;
+
 
 	public DumbAI(Player me) {
 		super(me);
-		this.row = 0;
-		this.column = 0;
 	}
 
 	protected @Override Location nextMove(Game g) {
@@ -37,32 +32,17 @@ public class DumbAI extends Controller {
 			if (g.getBoard().get(loc) == null && available.size() == 0) {
 				available.add(loc);
 			}
-			
-			//iterate backward or forward?
-			for (int i = 0; i < available.size(); i++) {
-				if ((g.getBoard().get(loc)== null) && (loc.row < available.get(i).row) && (loc.col < available.get(i).col)) {
-					available.add(i-1, loc);
-					i-=2;
-				} else if ((g.getBoard().get(loc)== null) && (loc.row == available.get(i).row) && (loc.col == available.get(i).col-1))  {
-					available.add(i-1,loc);
-					i-=2;
-				} else if ((g.getBoard().get(loc)== null) && (loc.row == available.get(i).row +1) && (loc.col == available.get(i).col-1))  {
-					available.add(i-1,loc);
-					i-=2;
-				} else {
-					available.add(loc);
-				}
-			}
 		}
 		
 		// wait a bit
 		delay();
 
-		// choose a random move
-		if (!available.isEmpty())
-			//return available.get(random.nextInt(available.size()));
+		// choose the smallest column number from the smallest row number
+		if (!available.isEmpty()) {
 			return available.get(0);
 
-		return null;
+		} else {
+			return null;
+		}
 	}
 }
