f9f8c33d780c96c1d4e3713aff59490774e7ae9f
Sophia <Sophia@dhcp-rhodes-1744.redrover.cornell.edu>
2019-07-15 19:47:07 -0400

    Pair programmed with Sophia He: changed javadoc comment for calcScore

--- A3/src/controller/MinMaxAI.java
+++ A3/src/controller/MinMaxAI.java
@@ -97,7 +97,7 @@ public abstract class MinMaxAI extends Controller {
 	}
 	
 	/**
-	 * Helper function to find best location.
+	 * Helper function to find best score in order to find best location for nextMove.
 	 * 
 	 * @param g the game to be played
 	 * @param depth the initial depth to be recursively implemented down to 0
